fabf0239599896588e11fd0f35c9ef8e438b0a9c
Nazeem <nazeem.s2k@gmail.com>
2021-07-25 13:44:46 +0200

    2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,27 +3,36 @@ import Contact from './components/Contact'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { 
+      name: 'Arto Hellas',
+      number: '040-1234567' 
+    }
+  ])
   const [ newName, setNewName ] = useState('')
-
+  const [ newNumber, setNewNumber ] = useState('')
+  
   const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   const addContact = (event) => {
     event.preventDefault()
 
     const names = persons.map(contact => contact.name)
+    const numbers = persons.map(contact => contact.number)
 
-    if (names.includes(newName)) {
-      window.alert(`${newName} is already added to phonebook`)
+    if (names.includes(newName) || numbers.includes(newNumber)) {
+      window.alert(`${newName} is already added to phonebook or the given number already exists`)
     }
 
     else {
       const contactObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
+
       setPersons(persons.concat(contactObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -36,6 +45,13 @@ const App = () => {
           <input 
             value={newName}
             onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: 
+          <input
+            value={newNumber} 
+            onChange={handleNumberChange}
           />
         </div>
         <div>
