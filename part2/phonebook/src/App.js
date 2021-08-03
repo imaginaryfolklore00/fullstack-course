54746db7a0040c038c45ab43f9ec03d216d3bd79
Nazeem <nazeem.s2k@gmail.com>
2021-08-03 20:20:38 +0200

    2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,17 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import ShowContacts from './components/ShowContacts'
 import contactService from './services/contacts'
+import Notification from './components/Notification'
+import Error from './components/Error'
+import './index.css'
 
 const App = () => {
   const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filterName, setFilterName] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
@@ -21,8 +26,20 @@ const App = () => {
       .update(contact.id, changedContact)
       .then(returnedContact => {
         setPersons(persons.map(person => person.id !== contact.id ? person : returnedContact))
+        setNewName('')
+        setNewNumber('')
+        setNotificationMessage(`Updated ${returnedContact.name}`)
+        setTimeout(() => {
+          setNotificationMessage(null)
+        }, 5000)
       })
-  }
+      .catch (error => {
+        setErrorMessage(`${changedContact.name} was already removed from server`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      })
+  } 
 
   const addContact = (event) => {
     event.preventDefault()
@@ -48,6 +65,10 @@ const App = () => {
           setPersons(persons.concat(returnedContact))
           setNewName('')
           setNewNumber('')
+          setNotificationMessage(`Added ${returnedContact.name}`)
+          setTimeout(() => {
+            setNotificationMessage(null)
+          }, 5000)
         })
       }
   }
@@ -73,6 +94,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notificationMessage} />
+      <Error message={errorMessage} />
       <Filter filterName={filterName} handleFilterChange={handleFilterChange} />
       <h3>Add contact</h3>
       <PersonForm newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} addContact={addContact} />
